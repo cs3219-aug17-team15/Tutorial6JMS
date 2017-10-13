c7f3c4c068d98d7bb3562063a82213689926c6d6
Xue Si Tan <tanxs93@gmail.com>
2017-10-13 20:05:20 +0800

    test jmspipe

--- Code-Tutorial-6/code/src/cs3219/jms/order/JmsPipe.java
+++ Code-Tutorial-6/code/src/cs3219/jms/order/JmsPipe.java
@@ -18,84 +18,96 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 /**
- * Matric 1:A0138474X
- * Name   1:Jong Xue Min Shermine
+ * Matric 1: A0138474X
+ * Name 1: Jong Xue Min Shermine
  *
  * Matric 2:
- * Name   2:
+ * Name 2:
  *
  * This file implements a pipe that transfer messages using JMS.
  */
 
-public class JmsPipe implements IPipe,MessageListener {
+public class JmsPipe implements IPipe, MessageListener {
 
-	private String factoryName, queueName;
-	private QueueConnectionFactory qconFactory;
-    private QueueConnection qcon;
-    private QueueSession qsession;
-    private QueueSender qsender;
-    private QueueReceiver qreceiver;
-    private Queue queue;
-    private TextMessage msg;
+  private String factoryName, queueName;
+  private QueueConnectionFactory qconFactory;
+  private QueueConnection qcon;
+  private QueueSession qsession;
+  private QueueSender qsender;
+  private QueueReceiver qreceiver;
+  private Queue queue;
+  private TextMessage msg;
 
-    public JmsPipe(String factoryName, String queueName) throws Exception{
-    	this.factoryName = factoryName;
-    	this.queueName = queueName;
-        init();
+  public JmsPipe(String factoryName, String queueName) throws Exception {
+    this.factoryName = factoryName;
+    this.queueName = queueName;
+    init();
+  }
+
+  private static InitialContext getInitialContext() throws NamingException {
+    Properties props = new Properties();
+    props.put(Context.INITIAL_CONTEXT_FACTORY, "org.jnp.interfaces.NamingContextFactory");
+    props.put(Context.PROVIDER_URL, "jnp://localhost:1099");
+    props.put(Context.URL_PKG_PREFIXES, "org.jboss.naming:org.jnp.interfaces");
+    return new InitialContext(props);
+  }
+
+  public void init() throws NamingException, JMSException {
+    InitialContext ctx = getInitialContext();
+    qconFactory = (QueueConnectionFactory) ctx.lookup(factoryName);
+    qcon = qconFactory.createQueueConnection();
+    qsession = qcon.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+    queue = (Queue) ctx.lookup(queueName);
+    qreceiver = qsession.createReceiver(queue);
+    qreceiver.setMessageListener(this);
+    qsender = qsession.createSender(queue);
+    msg = qsession.createTextMessage();
+    qcon.start();
+  }
+
+  public void write(Order s) {
+    try {
+      msg.setText(s.toString());
+      qsender.send(msg);
+    } catch (JMSException e) {
+      System.err.println("An exception has occurred: " + e.getMessage());
     }
-    private static InitialContext getInitialContext()
-            throws NamingException {
-        Properties props = new Properties();
-        props.put(Context.INITIAL_CONTEXT_FACTORY, "org.jnp.interfaces.NamingContextFactory");
-        props.put(Context.PROVIDER_URL, "jnp://localhost:1099");
-        props.put(Context.URL_PKG_PREFIXES, "org.jboss.naming:org.jnp.interfaces");
-        return new InitialContext(props);
-    }
-    public void init()
-            throws NamingException, JMSException {
-    	InitialContext ctx = getInitialContext();
-        qconFactory = (QueueConnectionFactory) ctx.lookup(factoryName);
-        qcon = qconFactory.createQueueConnection();
-        qsession = qcon.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
-        queue = (Queue) ctx.lookup(queueName);
-        qreceiver = qsession.createReceiver(queue);
-        qsender = qsession.createSender(queue);
+  }
+
+  public Order read() {
+    try {
+      if (msg.getText() != null && !msg.getText().isEmpty()) {
+        String val = msg.getText();
         msg = qsession.createTextMessage();
-        qcon.start();
+        return Order.fromString(val);
+      }
+    } catch (JMSException e) {
+      e.printStackTrace();
     }
-	public void write(Order s) {
-        try {
-        	msg.setText(s.toString());
-			qsender.send(msg);
-		} catch (JMSException e) {
-			System.err.println("An exception has occurred: " + e.getMessage());
-		}
-	}
+    return null;
+  }
 
-	public Order read() {
-		return Order.fromString(msg.toString());
-	}
+  public void close() {
+    try {
+      qsender.close();
+      qsession.close();
+      qcon.close();
+    } catch (JMSException e) {
+      System.err.println("An exception has occurred: " + e.getMessage());
+    }
+  }
 
-	public void close() {
-        try {
-        	qsender.close();
-            qsession.close();
-			qcon.close();
-		} catch (JMSException e) {
-			System.err.println("An exception has occurred: " + e.getMessage());
-		}
-	}
-	public void onMessage(Message arg0) {
-	try{
-		if (arg0 instanceof TextMessage) {
-            msg.setText(((TextMessage) arg0).getText());
-        } else {
-            msg.setText(arg0.toString());
-        }
+  public void onMessage(Message arg0) {
+    try {
+      if (arg0 instanceof TextMessage) {
+        msg.setText(((TextMessage) arg0).getText());
+      } else {
+        msg.setText(arg0.toString());
+      }
     } catch (JMSException jmse) {
-        System.err.println("An exception occurred: " + jmse.getMessage());
+      System.err.println("An exception occurred: " + jmse.getMessage());
     }
 
-	}
+  }
 
 }
